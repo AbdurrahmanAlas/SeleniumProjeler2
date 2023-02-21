aee436da5e835fdc90f9ff1b68f4cf78a80e45ab
Abdurrahman Alas <aalas@kbb.local>
2023-02-21 09:49:40 +0300

    c17

--- src/test/java/API_CALISMALARI/C16_BaseUrlHerokuapp.java
+++ src/test/java/API_CALISMALARI/C16_BaseUrlHerokuapp.java
@@ -1,8 +1,10 @@
 package API_CALISMALARI;
 
 import baseURL.HerokuappBaseurl;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.hamcrest.Matchers;
+import org.json.JSONObject;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
@@ -49,4 +51,64 @@ public class C16_BaseUrlHerokuapp extends HerokuappBaseurl {
 
 
 
-}}
+}
+
+    @Test
+    public void post01() {
+
+    /*
+        2- https://restful-booker.herokuapp.com/booking endpointine asagidaki
+         body’ye sahip bir
+         POST request gonderdigimizde
+         donen response’un
+          status code’unun 200 oldugunu ve
+           “firstname” degerinin “Ali” oldugunu test edin
+
+
+
+      {
+      "firstname" : "Ali",
+      "lastname" : “Bak",
+      "totalprice" : 500,
+      "depositpaid" : false,
+      "bookingdates" : {
+                    "checkin" : "2021-06-01",
+                    "checkout" : "2021-06-10"
+                    },
+      "additionalneeds" : "wi-fi"
+       }
+      */
+        // 1 - URL ve body hazirla
+        specHerokuapp.pathParam("pp1", "booking");
+
+        JSONObject innerBody = new JSONObject();
+        JSONObject reqBody = new JSONObject();
+
+        innerBody.put("checkin", "2021-06-01");
+        innerBody.put("checkout", "2021-06-10");
+
+        reqBody.put("firstname", "Ali");
+        reqBody.put("lastname", "Ali");
+        reqBody.put("totalprice", 500);
+        reqBody.put("depositpaid", false);
+        reqBody.put("bookingdates", innerBody);
+        reqBody.put("additionalneeds", "wi-fi");
+
+
+        //2- Expected Data hazirla
+        //3- Response u kaydet
+
+        Response response = given().
+                spec(specHerokuapp).
+                contentType(ContentType.JSON).when().
+                body(reqBody.toString()).
+                post("/{pp1}");
+
+        // 4-Assertion
+
+        response.then().assertThat().statusCode(200).
+                body("booking.firstname", Matchers.equalTo("Ali"));
+
+
+    }
+}
